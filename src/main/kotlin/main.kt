5efb7cd0f438e009f2bc9c3ecda4ea35edbe2045
strax1e <strax1e@yandex.ru>
2021-07-08 02:38:07 +0300

    server: improved logging

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,16 @@
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.io.BufferedWriter
+import java.io.BufferedReader
 import java.io.OutputStreamWriter
+import java.io.OutputStream
 import java.net.ServerSocket
 import java.net.Socket
 import java.sql.SQLException
+import java.text.SimpleDateFormat
+import java.util.GregorianCalendar
 
 fun main(args: Array<String>) {
     val dbHandler = DbHandler("jdbc:sqlite:${args[0]}", org.sqlite.JDBC()) // creating a db connection
@@ -15,7 +19,7 @@ fun main(args: Array<String>) {
     // Main loop which serves clients
     while (!serverSocket.isClosed) {
         val clientSocket: Socket = serverSocket.accept() // waiting for connections
-        println("${clientSocket.inetAddress}:${clientSocket.port} is connected")
+        newLog("Client is connected", clientSocket)
 
         CoroutineScope(Dispatchers.IO).launch {
             serveClient(clientSocket, dbHandler)
@@ -34,19 +38,18 @@ fun serveClient(clientSocket: Socket, dbHandler: DbHandler) {
     try {
         val writer = OutputStreamWriter(clientSocket.getOutputStream())
         val reader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
-
         val request: String = reader.readLine()
-        println("Got: $request")
+        newLog("Got: $request", clientSocket)
 
-        val response = getResponse(request, dbHandler)
+        val response = getResponse(request, dbHandler, clientSocket)
         writer.write(response)
         writer.flush()
-        println("Sent: $response")
+        newLog("Sent: $response", clientSocket)
     } catch (e: Exception) {
-        println("Exception: ${e.message}. Closing connection.")
+        newLog("Exception: ${e.message}. Closing connection.", clientSocket)
     } finally {
         clientSocket.close()
-        println("The connection to ${clientSocket.inetAddress}:${clientSocket.port} is closed")
+        newLog("The connection is closed", clientSocket)
     }
 }
 
@@ -56,12 +59,29 @@ fun serveClient(clientSocket: Socket, dbHandler: DbHandler) {
  * @param[dbHandler] database handler
  * @return required data from database
  */
-fun getResponse(request: String, dbHandler: DbHandler): String {
+fun getResponse(request: String, dbHandler: DbHandler, clientSocket: Socket): String {
     return try {
         if (!request.startsWith("select")) throw SQLException("Not a select query")
         dbHandler.getData(request)
     } catch (e: SQLException) {
-        println("getResponse() Exception: ${e.message}")
+        newLog("getResponse() exception: ${e.message}", clientSocket)
         "err"
     }
 }
+
+/**
+ * Makes new log in output stream
+ * @param[stream] output stream
+ */
+fun newLog(message: String, clientSocket: Socket, stream: OutputStream = System.out) {
+    val writer = BufferedWriter(OutputStreamWriter(stream))
+    writer.write("${getCurrentTime()}: ${clientSocket.remoteSocketAddress}: $message")
+    writer.newLine()
+    writer.flush()
+}
+
+/**
+ * Gets a current time for logging
+ * @return current time. "dd.MM.yy HH:mm:ss" is a format
+ */
+fun getCurrentTime(): String = SimpleDateFormat("dd.MM.yy HH:mm:ss").format(GregorianCalendar().time)
